de459fe8acbdabf271481f734e7817e6a18e0e68
Baiba Cābele <baibacabele@gmail.com>
2026-09-23 22:47:46 +0300

    Start menu with animated sprite completed

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public Button startButton;
+    public Animator fadeAnim;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        startButton.Select();
+    }
+
+    public void FadeOut()
+    {
+        fadeAnim.SetBool("AlphaFade", true);
+
+        StartCoroutine(WaitForFade());
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private IEnumerator WaitForFade()
+    {
+        yield return new WaitForSeconds(1f);
+
+        LoadScene("CityScene");
+    }
+}
